db554db0538a4f98dcecdd6b451c2b37d8d5a1d3
shuja-git <shuja.devops@gmail.com>
2025-06-14 20:18:01 +0530

    added a check_status function for status in mysql and backend

--- frontend.sh
+++ frontend.sh
@@ -2,24 +2,30 @@ source common.sh
 
 Print_Headings "Install Nginx"
 dnf install nginx -y &>>/tmp/expense.log
+check_status $?
 
 Print_Headings "Start Nginx"
 systemctl enable nginx &>>/tmp/expense.log
 systemctl start nginx &>>/tmp/expense.log
+check_status $?
 
 Print_Headings "Copy expense configuration"
 cp expense.conf /etc/nginx/default.d/expense.conf
+check_status $?
 
 Print_Headings "Remove Old contents"
 rm -rf /usr/share/nginx/html/*
+check_status $?
 
 Print_Headings "Download App contents"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
+check_status $?
 
 Print_Headings "Extract app contents"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/expense.log
+check_status $?
 
 Print_Headings "Re-Start Nginx"
 systemctl restart nginx
-
+check_status $?
--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,15 @@
+Print_Headings "Install mysql"
 dnf install mysql-server -y &>>/tmp/expense.log
+check_status $?
+
+Print_Headings "Enable mysql"
 systemctl enable mysqld &>>/tmp/expense.log
+check_status $?
+
+Print_Headings "Start mysql"
 systemctl start mysqld &>>/tmp/expense.log
+check_status $?
+
+Print_Headings "Setup mysql password"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
+check_status $?
