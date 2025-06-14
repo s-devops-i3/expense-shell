b8fb1127843e8b2f27f04d7752bd1cf1453a365b
shuja-git <shuja.devops@gmail.com>
2025-06-14 17:46:59 +0530

    function added for headings

--- backend.sh
+++ backend.sh
@@ -1,61 +1,65 @@
 mysql_root_password=$1
 
-echo "Disable Nodejs"
+Print_Headings() {
+  echo $1
+  echo "############ $1 ##########" $>>/tmp/expense.log
+}
+Print_Headings "Disable Nodejs"
 dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo "Enable nodejs V20"
+Print_Headings "Enable nodejs V20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
 
-echo "Install nodejs"
+Print_Headings "Install nodejs"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo "Copy Service file"
+Print_Headings "Copy Service file"
 cp backend.service /etc/systemd/system/backend.service
 echo $?
 
-echo "Remove old content"
+Print_Headings "Remove old content"
 rm -rf /app
 echo $?
 
-echo "Adding expense user"
+Print_Headings "Adding expense user"
 useradd expense &>>/tmp/expense.log
 echo $?
 
-echo "Creating App dir"
+Print_Headings "Creating App dir"
 mkdir /app
 echo $?
 
-echo "Downloading content"
+Print_Headings "Downloading content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
 cd /app
 echo $?
 
-echo "Extracting app contents"
+Print_Headings "Extracting app contents"
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
 
 cd /app
 echo $?
 
-echo "Installing dependencies"
+Print_Headings "Installing dependencies"
 npm install &>>/tmp/expense.log
 echo $?
 
-echo "Starting backend"
+Print_Headings "Starting backend"
 systemctl daemon-reload &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 echo $?
-echo "install MySQL client"
+Print_Headings "install MySQL client"
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
 
-echo "Load Schema"
+Print_Headings "Load Schema"
 mysql -h 172.31.27.41 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
 
