3026380db2f25d7d5afcd6ded8e34c95f7aa10d9
shuja-git <shuja.devops@gmail.com>
2025-06-14 15:50:24 +0530

    redirected output to expense.log file

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,13 @@
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+dnf install nginx -y &>>/tmp/expense.log
+systemctl enable nginx &>>/tmp/expense.log
+systemctl start nginx &>>/tmp/expense.log
 
 cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 
 systemctl restart nginx
 
